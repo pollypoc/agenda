b5eb653b5a683701050a111138e9abd626b8c420
pollypoc <lucastecinfo17@gmail.com>
2018-09-25 21:15:51 -0300

    Finalizaçao do projeto de agenda

--- events/models.py
+++ events/models.py
@@ -3,41 +3,47 @@ from django.utils import timezone
 from libgravatar import Gravatar
 
 
+# Create your models here.
 class Event(models.Model):
+    """Classe contendo o evento propriamente dito, sua data, descrição
+    e também prioridade."""
 
     priorities_list = (
-                       ('0', 'Sem prioridade'),
-                       ('1', 'Normal'),
-                       ('2', 'Urgente'),
-                       ('3', 'Muito Urgente'),
-                       ('4', 'Ultra Mega Hiper Urgente'))
+        ('0', 'Sem prioridade'),
+        ('1', 'Normal'),
+        ('2', 'Urgente'),
+        ('3', 'Muito Urgente'),
+    )
 
     date = models.DateField()
-    event = models.CharField(max_length=100)
-    priority = models.CharField(max_length=1, choices= priorities_list)
+    event = models.CharField(max_length=80)
+    priority = models.CharField(max_length=1, choices=priorities_list)
 
     class Meta:
-        ordering = ("-date", "-priority", "event")
+        ordering = ('-date', '-priority', 'event',)
 
     def number_of_comments(self):
+        """Retorna a quantidade de comentários dentro de um evento."""
         return self.comment_event.count()
 
     def __str__(self):
         return self.event
 
+
 class Comment(models.Model):
-    """Comentário efetuado em um evento"""
+    """Comentários efetuados em um determinado evento."""
 
     author = models.CharField(max_length=80)
     email = models.EmailField()
-    text = models.CharField(max_length=240)
+    text = models.CharField(max_length=160)
     commented = models.DateTimeField(default=timezone.now)
     event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='comment_event')
 
     def avatar(self):
-        """RETORNA A PARTIR DO ENDEREÇO DE EMAIL, UM AVATAR CONFIGURADO NO GRAVATAR"""
+        """Retorna a partir do endereço de e-mail, um avatar
+        configurado no Gravatar ou um dos avatares padrão deles."""
         g = Gravatar(self.email)
-        return g.get_image(default ='identicon')
+        return g.get_image(default='identicon')
 
     def __str__(self):
-        return "{} comentou em {:%c}". format(self.author, self.commented)
+        return "{} commentou em {:%c}".format(self.author, self.commented)
--- events/views.py
+++ events/views.py
@@ -1,33 +1,37 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Event, Comment
-from django.utils.timezone import localdate
 from django.core.paginator import Paginator, InvalidPage
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from django.utils.timezone import localdate
 from django.views.defaults import bad_request, server_error
-from datetime import datetime,timedelta
+from .models import Event, Comment
 from .forms import EventForm, CommentForm
+
+from datetime import datetime, timedelta
 
 ITEMS_PER_PAGE = 5
 
 def split_date(string_date):
-    """Transforma a data em YYYY-MM-DD em uma tupla de três valores para utilizar
-    na visão de eventos de um determinado dia."""
+    """Transforma a data em YYYY-MM-DD em uma tupla de três valores para
+    utilizar na visão de eventos de um determinado dia."""
     for value in string_date.split('-'):
         yield int(value)
 
-# Create your views here.
 
+# Create your views here.
 def index(request):
-    """ Exibe a pagina inicial da aplicação"""
+    """Exibe a página principal da aplicaão."""
     context = {
         'hide_new_button': True,
-        'priorities': Event.priorities_list,
-        'today': localdate(),
+            'priorities': Event.priorities_list,
+            'today': localdate(),
     }
     return render(request, 'index.html', context)
 
-"""EXIBE TODOS OS EVENTOS EM UMA UNICA PAGINA, RECEBE O NUMERO DA PAGINA A SER VISUALIZADA VIA GET"""
+
 def all(request):
+    """Exibe todas os eventos consolidados em uma única página, recebe o
+    número da página a ser visualizada via GET."""
+
     page = request.GET.get('page', 1)
     paginator = Paginator(Event.objects.all(), ITEMS_PER_PAGE)
     total = paginator.count
@@ -37,45 +41,46 @@ def all(request):
     except InvalidPage:
         events = paginator.page(1)
 
-
     context = {
         'events': events,
-        'total': total,
-        'priorities': Event.priorities_list,
-        'today': localdate(),
+            'total': total,
+            'priorities': Event.priorities_list,
+            'today': localdate(),
     }
-
     return render(request, 'events.html', context)
 
-""" VISUALIZAÇÃO DOS EVENTOS DE UM DETERMINADO DIA, RECEBE A DATA EM FORMATO DE ANO/MES/DIA COMO PARÂMETRO"""
-def day(request, year:int, month:int, day:int):
-    day = datetime(year, month, day)
-    events = Event.objects.filter(
-        date='{:%Y-%m-%d}'.format(day)).order_by('-priority', 'event')
 
+def day(request, year: int, month: int, day: int):
+    """Visualização dos eventos de um determinado dia, recebe a data em
+    formato ano/mês/dia como parâmtro."""
+    day = datetime(year, month, day)
     context = {
         'today': localdate(),
-        'day': day,
-        'event': day + timedelta(days=1),
-        'previous': day - timedelta(days=1),
-        'priorities': Event.priorities_list,
+            'day': day,
+            'events': Event.objects.filter(
+                date='{:%Y-%m-%d}'.format(day)).order_by('-priority', 'event'),
+            'next': day + timedelta(days=1),
+            'previous': day - timedelta(days=1),
+            'priorities': Event.priorities_list,
     }
     return render(request, 'day.html', context)
 
+
 def delete(request, id: int):
-    """Apaga um evento especifico, se o evento não existir resultará em erro 404, se algo
-    errado ocorrer retornará a página de erro."""
+    """Apaga um evento específico, se o evento não existir resultará em erro
+    404, se algo errado ocorrer retornará a página de erro."""
     event = get_object_or_404(Event, id=id)
     (year, month, day) = tuple(
         split_date('{:%Y-%m-%d}'.format(event.date)))
     if event.delete():
-        return redirect('agenda-events', year=year, month=month, day=day)
+        return redirect('agenda-events-day', year=year, month=month, day=day)
     else:
-        return server_error(request, 'ops_500.html')
+        return server_errror(reqest, 'ops_500.html')
+
 
 def edit(request):
     """Edita o conteúdo de um evento, recebendo os dados enviados pelo
-    formulário, validando e populando em evento já existente."""
+    formulário, validando e populando em um evento já existente."""
     form = EventForm(request.POST)
     if form.is_valid():
         event = get_object_or_404(Event, id=request.POST['id'])
@@ -87,36 +92,39 @@ def edit(request):
             split_date('{:%Y-%m-%d}'.format(event.date)))
         return redirect('agenda-events-day', year=year, month=month, day=day)
     else:
-        bad_request(request, None, 'ops_400.html')
+        return bad_request(request, None, 'ops_400.html')
+
 
 def new(request):
-    """ Recebe os dados de um novo evento via POST, faz a validação dos dados
+    """Recebe os dados de um novo evento via POST, faz a validação dos dados
     e aí insere na base de dados."""
-    form =  EventForm(request.POST)
+    form = EventForm(request.POST)
     if form.is_valid():
         form.save(commit=True)
-
+        # uso a data enviada pelo formulário para o redirecionamento.
         (year, month, day) = tuple(
             split_date(request.POST['date']))
         return redirect('agenda-events-day', year=year, month=month, day=day)
     else:
         return bad_request(request, None, 'ops_400.html')
 
+
 def show(request, id: int):
     """Visualização de um determinado evento e de seus comentários, recebe
     o 'id' do evento. Caso seja acessado via POST insere um novo comentário."""
     event = get_object_or_404(Event, id=id)
+
     if request.method == "POST":
         form = CommentForm(request.POST)
         if form.is_valid:
             form.save()
-            return redirect('agenda-events-day', id=id)
-    context={
-        'event': event,
-        'comments': Comment.objects.filter(event=id).order_by('-commented'),
-        'hide_new_button':True,
-        'prioriries': Event.priorities_list,
-        'today': localdate(),
-    }
+            return redirect('agenda-events-show', id=id)
 
+    context = {
+        'event': event,
+            'comments': Comment.objects.filter(event=id).order_by('-commented'),
+            'hide_new_button': True,
+            'priorities': Event.priorities_list,
+            'today': localdate(),
+    }
     return render(request, 'show.html', context)
