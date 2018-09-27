08cb76044d96a46b162a5c1cf389f35fc7ab8b5a
pollypoc <lucastecinfo17@gmail.com>
2018-09-27 20:21:29 -0300

    Criando admin de blog

--- agenda/settings.py
+++ agenda/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 INSTALLED_APPS = [
     'events',
+    'blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
