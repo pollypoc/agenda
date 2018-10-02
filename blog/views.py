dab053cb21be4237414a08c5fd9f198f8070ea2c
pollypoc <lucastecinfo17@gmail.com>
2018-10-02 19:55:28 -0300

    Finalizando a listagem de eventos em new post

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,10 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404, redirect
 from .forms import PostForm
 
+from django.utils.timezone import localdate
+from datetime import datetime
+from events.models import Event
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -25,8 +29,22 @@ def post_new(request):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+        day = datetime(localdate().year, localdate().month, localdate().day)
+        context = {
+            'events': Event.objects.filter(
+                date='{:%Y-%m-%d}'.format(day)).order_by('-priority', 'event'),
+            'form': form
+        }
+    return render(request, 'blog/post_edit.html', context)
 
+def day():
+    """Visualização dos eventos de um determinado dia, recebe a data em
+    formato ano/mês/dia como parâmtro."""
+    day = datetime(localdate().year, localdate().month, localdate().day)
+    context = {
+            'events': Event.objects.filter(
+                date='{:%Y-%m-%d}'.format(day)).order_by('-priority', 'event'),
+    }
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -40,4 +58,10 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+        day = datetime(localdate().year, localdate().month, localdate().day)
+        context = {
+            'events': Event.objects.filter(
+                date='{:%Y-%m-%d}'.format(day)).order_by('-priority', 'event'),
+            'form': form
+        }
+    return render(request, 'blog/post_edit.html', context)
